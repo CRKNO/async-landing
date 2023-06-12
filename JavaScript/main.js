450876fd2ed0fcf8b71fbea0eb533ec64f3bd48d
CRKNO <cristiancanosantiago@hotmail.com>
2023-06-12 20:48:11 -0300

    Updates

---
+++ JavaScript/main.js
@@ -0,0 +1,145 @@
+// import fetch from "node-fetch";
+const APIVIDEOS = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCi_zKr64k8WIx8miV36rr1w&part=snippet%2Cid&type=video&order=date&maxResults=';
+const APIMOSTPOPULAR = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCi_zKr64k8WIx8miV36rr1w&part=snippet&type=video&chartmostpopular&maxResults=';
+
+const mostPopularSection = document.querySelector(".most-popular");
+const lastVideosSection = document.querySelector(".last-videos");
+const allVideosSection = document.querySelector(".all-videos");
+const seriesSection = document.querySelector(".series");
+const allSections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-items");
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '42283b2d77msh9c22c331a971763p17bd53jsnedb20129b361',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+}
+
+let activeView = "";
+let pageToken = "";
+let recived = [];
+
+function createCards(video){
+
+    let div = document.createElement("div");
+    div.classList.add("video-card");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", video.snippet.thumbnails.high.url);
+    img.setAttribute("alt", video.snippet.title);
+
+    let p = document.createElement("p");
+    p.classList.add("card-title");
+    p.textContent = video.snippet.title;
+
+    div.append(img, p);
+    return div;
+}
+
+function putVideos(newVideos, videosContainerSection){
+    // console.log(newVideos)
+    for(video of newVideos.items){
+        if(activeView === "videos") {recived.push(video)};
+        let aux = createCards(video);
+        aux.addEventListener("click", ()=>{
+            window.location = `https://www.youtube.com/watch?v=${video.id.videoId}`
+        })
+        videosContainerSection.append(aux);
+    }
+}
+
+function removeVideos(oldVideos){
+    for(let video of oldVideos){
+        video.remove();
+    }
+}
+
+function removeVideosOfSection(section){
+    const videos = section.querySelectorAll(".video-card");
+    for(let video of videos){
+        video.remove();
+    }
+}
+
+async function fetchAllVideos(pgToken){
+    pgToken = pgToken || "";
+    const allVideos = await ((await fetch(`${APIVIDEOS}10&pageToken=${pgToken}`, options)).json());
+    pageToken = allVideos.nextPageToken;
+    if(activeView != "videos"){
+        activeView = "videos";
+        removeVideosOfSection(allVideosSection);
+    }
+    putVideos(allVideos, allVideosSection);
+}
+
+
+
+(async () =>{ //auto-executable anonimous function
+    try{
+        const videos = await (await fetch(`${APIVIDEOS}8`, options)).json();
+        const popularVideos = await (await fetch(`${APIMOSTPOPULAR}4`, options)).json();
+        const popularVideosPlaceholders = document.querySelectorAll(".popular-videos-placeholder");
+        const lastVideosPlaceholders = document.querySelectorAll(".last-videos-placeholder");
+
+        putVideos(popularVideos, mostPopularSection);
+        removeVideos(popularVideosPlaceholders);
+        
+        putVideos(videos, lastVideosSection);
+        removeVideos(lastVideosPlaceholders);
+
+        const cards = null || document.querySelectorAll(".video-card");
+    }
+    catch (error){
+        console.log(error);
+    }
+})();
+
+for(item of navItems){
+    item.addEventListener("click", (e)=>{
+        window.scroll(0, 0);
+        let target = e.target;
+        switch(target.textContent){
+            case "Home":
+                activeView = "home";
+                for(let section of allSections){
+                    if(section.classList.contains("most-popular") || section.classList.contains("last-videos")){
+                        section.classList.remove("inactive");
+                    }
+                    else{
+                        section.classList.add("inactive");
+                    }
+                }
+
+                break;
+            case "Videos":
+                for(let section of allSections){
+                    if(section.classList.contains("all-videos")){
+                        section.classList.remove("inactive");
+                    }
+                    else{
+                        section.classList.add("inactive");
+                    }
+                }
+                fetchAllVideos();
+                break;
+            case "Series":
+                for(let section of allSections){
+                    if(section.classList.contains("series")){
+                        section.classList.remove("inactive");
+                    }
+                    else{
+                        section.classList.add("inactive");
+                    }
+                }
+                break;
+        }
+    });
+}
+
+window.addEventListener("scroll", (e) =>{
+    if(window.scrollY + window.innerHeight >= document.body.clientHeight && activeView === "videos"){
+        fetchAllVideos(pageToken);
+    }
+})
